965632c35a5a9b8f3f67c4c4f904072ac9dc5f16
tomzcn123 <79131150+tomzcn123@users.noreply.github.com>
2023-04-26 01:30:03 -0400

    Update Stock_C.py

--- Stock_C.py
+++ Stock_C.py
@@ -1,32 +1,13 @@
 import streamlit as st
-import yfinance as yf
 import pandas as pd
-from ta.trend import MACD
-import lxml
-from collections import defaultdict
-import plotly.graph_objects as go
-from plotly.subplots import make_subplots
 import requests
-import requests
-import tempfile
+from io import BytesIO
 
 github_url = 'https://raw.githubusercontent.com/tomzcn123/china_stock/main/A.xlsx'
-
-# Download the Excel file
 response = requests.get(github_url)
+excel_data = BytesIO(response.content)
+sheet_name = 'Sheet'
+data = pd.read_excel(excel_data, sheet_name=sheet_name, engine='openpyxl')
+tickers = data[['tickers', 'sector']].to_dict('records')
 
-# Check if the download was successful
-if response.status_code == 200:
-    # Save the downloaded file as a temporary file
-    with tempfile.NamedTemporaryFile(suffix=".xlsx") as temp_file:
-        temp_file.write(response.content)
-        temp_file.flush()
-
-        # Read the Excel file
-        sheet_name = 'Sheet'  # Replace 'Sheet' with the name of the sheet containing the data
-        data = pd.read_excel(temp_file.name, sheet_name=sheet_name, engine='openpyxl')
-
-        # Process the data
-        tickers = data[['tickers', 'sector']].to_dict('records')
-else:
-    print("Error: Unable to download the Excel file.")
+st.write(tickers)
